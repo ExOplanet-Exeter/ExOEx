c95218555cef62aae8714dca49dc54cd00b259a4
FreddyWordingham <fjmw201@exeter.ac.uk>
2015-02-11 12:42:14 +0000

    Parallel testing

--- Testing/Freddy/main.c
+++ Testing/Freddy/main.c
@@ -3,10 +3,14 @@
 int main(int argc, char **argv)
 { 
 
+  int i = 0, j= 0;
    #pragma omp parallel
   {
-    printf("Hello OpenMP!\n");
+    for (i;i<100;i++)
+    printf("%i\n");
+    j++;
   }
+  printf("%i\n",j);
 
   return 0;
 }
