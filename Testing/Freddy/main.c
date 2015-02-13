10046eadc7317fdce44466a47dd03512b43b5d2c
Freddy <fjmw201@exeter.ac.uk>
2015-02-13 00:17:38 +0000

    Parallelisation omitimisations.

--- Testing/Freddy/main.c
+++ Testing/Freddy/main.c
@@ -1,34 +1,6 @@
-#include <stdio.h>
-#include <omp.h>
+#include <gtk/gtk.h>
 
 int main(){
-  
-  int i, threadID;
-  int g_nLoops, p_nLoops;
-
-  g_nLoops = 0;
-  
-  #pragma omp parallel private(p_nLoops, threadID)
-  {
-    p_nLoops = 0;
-    threadID = omp_get_thread_num();
-    
-    #pragma omp for
-    for (i=0; i<100000; i++){
-      ++p_nLoops;
-    }
-    
-    #pragma omp critical
-      {
-      printf("Thread %i adding %i to sum (%i)\n",threadID,p_nLoops,g_nLoops);
-
-      g_nLoops = g_nLoops + p_nLoops;
-      
-      printf("Total loops = %i.\n\n",g_nLoops);
-      }
-  }
-  
-  printf("Final total loops = %i\n",g_nLoops);
   
   return 0;
 }
