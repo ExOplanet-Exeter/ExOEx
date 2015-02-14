8aad49f14e31d8dc603821994fd40ee52d0dcd8f
FreddyWordingham <fjmw201@exeter.ac.uk>
2015-02-14 20:35:06 +0000

    No major changes,

--- Arc/modules/functions/output.c
+++ Arc/modules/functions/output.c
@@ -38,27 +38,31 @@ void output(Datasystem *data,Planet *exo){
 		printf(ARED "ERROR! Could not open lightcurve.dat!\n" ARESET);
 	
 	// Fitting loop
+	double binMax = 0;
 	for (int n=0; n<180; n++){
 		data->fittedCurve[n] = data->lightCurve[n] / sin((n+0.5)*(PI/180.0));
+		if (data->fittedCurve[n] > binMax)
+			binMax = data->fittedCurve[n];
 	}
-
+	for (int n=0; n<180; n++){
+		data->fittedCurve[n] = data->fittedCurve[n] / binMax;
+	}
 	data->fittedCurve[0] = data->fittedCurve[1];
 	
-	
 	printf("Number dead = %i/%i\n(%lf%%)\n",data->nDead,exo->nPhot,(100.0*data->nDead)/exo->nPhot);
+	
 	// Printing loop
 	for (int n=0; n<180; n++){
 		fprintf(lightcurve,"%i %lf\n",n,data->fittedCurve[n]);
 		y[n] = data->fittedCurve[n];
 		x[n] = n;
 	}
-
 	gnuplot_ctrl * h1;
 	h1 = gnuplot_init();
 	gnuplot_cmd(h1,"set terminal dumb");
 	gnuplot_setstyle(h1,"lines");
 	gnuplot_cmd(h1,"set xrange [0:180]");
-	gnuplot_cmd(h1,"set yrange [0:200000]");
+	gnuplot_cmd(h1,"set yrange [0:1]");
 	gnuplot_plot_xy(h1,x,y,180,"user-defined points");
 	
 	gnuplot_close(h1);
