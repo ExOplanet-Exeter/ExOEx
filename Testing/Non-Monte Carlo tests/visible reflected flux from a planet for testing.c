5cc6d366b8d62cb069e78fb3ea99ff0f2fd6fbe8
Eshwen Bhal <milanman_eshb@hotmail.co.uk>
2015-03-16 21:21:51 +0000

    updated lambert scattering test program

--- Testing/Non-Monte Carlo tests/visible reflected flux from a planet for testing.c
+++ Testing/Non-Monte Carlo tests/visible reflected flux from a planet for testing.c
@@ -3,21 +3,20 @@
 #include <math.h>
 
 #define PI 3.14159265
-#define N_ROWS 1000
-#define N_COLS 1000
+#define N_ROWS 750
+#define N_COLS 750
 #define RADIUS_PLANET 1
 #define DIST_STAR 10    // Distance from planet to star
 
 /*
-- FIX BUG THAT'S GIVING ME A SLIGHTLY LARGER VISIBLE AREA FOR PHASE ANGLES > 180^o.
-- ADD UNITS TO PARAMETERS LIKE INTENSITY, FLUX, AREA AND DISTANCE?
-- INCLUDE ISOTROPIC AND RAYLEIGH SCATTERING (FIGURE OUT HOW TO READ IN SPECIFIC VALUES FOR THE
-  H-FUNCTION ETC. FOR A GIVEN MU (THAT'S ROUNDED TO COINCIDE WITH VALUES IN THE TABLES.
-*/
+ - INCLUDE ISOTROPIC AND RAYLEIGH SCATTERING (FIGURE OUT HOW TO READ IN SPECIFIC VALUES FOR THE
+ H-FUNCTION ETC. FOR A GIVEN MU (THAT'S ROUNDED TO COINCIDE WITH VALUES IN THE TABLES).
+ */
 
 typedef struct variables {
     double areaGrid[N_ROWS][N_COLS];    // array containing surface area elements of planet
-    double obsPhaseangle;               // Angle between observer and planet relative to star
+    double obsPhaseangle;               /* Angle between observer and planet relative to star
+										   set so maximum flux is at a phase angle of zero */
     double obsDistance;                 // Distance from observer to planet
     double obsDistance_x;               // x-coordinate of observer's position
     double obsDistance_y;               // y-coordinate of observer's position
@@ -43,33 +42,16 @@ void getAngles (Variables *);
 void getVisibleArea (Variables *);
 void getMu (Variables *, double, double, double);
 void getReceivedFlux (Variables *);
-double conductLambert (Variables *);
-double conductIso (Variables *);
-double conductRayleigh (Variables *);
+void conductLambert (Variables *);
+void conductIso (Variables *);
+void conductRayleigh (Variables *);
 
 int main () {
 	int choice;
 
 	Variables *A = newvariables(); // Initializes structure
 
-	printf("Enter a phase angle (in degrees) and distance (in units of the exoplanet's radius) "
-           "between the observer and exoplanet.\n");
-
-    scanf("%lg %lg", &A->obsPhaseangle, &A->obsDistance);
-    if ( A->obsPhaseangle < 0 || A->obsPhaseangle > 360 ) {
-        printf("Please enter a value between 0 and 360 for the phase angle.\n");
-        main();
-    }
-    else if ( A->obsDistance < 1 ) {
-        printf("Please enter an observation distance of 1 or greater.\n");
-        main();
-    }
-    else {
-        A->obsPhaseangle = A->obsPhaseangle * PI / 180;     // Converts phase angle to radians
-        A->obsDistance = A->obsDistance * RADIUS_PLANET;    // Scales observation distance
-        A->obsDistance_x = A->obsDistance * cos(A->obsPhaseangle);
-        A->obsDistance_y = A->obsDistance * sin(A->obsPhaseangle);
-    }
+	A->obsDistance = RADIUS_PLANET; // Arbitrary observation distance, makes no difference to result
 
     printf("For Lambert scattering, enter '1'.\n"
 		   "For isotropic scattering, enter '2'.\n"
@@ -80,12 +62,33 @@ int main () {
 		printf("Number is invalid, please choose again.\n");
 		main();
 	}
-	else if (choice == 1)
-		conductLambert(A);
-	else if (choice == 2)
-		conductIso(A);
-	else if (choice == 3)
-		conductRayleigh(A);
+	else {
+		int G;
+
+		FILE *outfile;
+		outfile = fopen("flux_array.txt","w"); // opens file for writing
+
+		for (G = 0 ; G <= 180 ; ++G) {
+			printf("phase angle = %i\n", G);
+			A->obsPhaseangle = (180 - G) * PI / 180; // Sets phase angle and converts from degrees to radians
+			A->obsDistance_x = A->obsDistance * cos(A->obsPhaseangle);
+			A->obsDistance_y = A->obsDistance * sin(A->obsPhaseangle);
+			if (choice == 1)
+				conductLambert(A);
+			/* else if (choice == 2)
+				conductIso(A);
+			else if (choice == 3)
+				conductRayleigh(A); */
+			double normalizer;
+
+			if (G == 0)
+				normalizer = A->flux_received; // gets the flux at zero phase angle to normalize phase curve
+
+			fprintf(outfile, "%i %g\n", G, A->flux_received / normalizer);
+		}
+		fclose(outfile);
+	}
+	return 0;
 }
 
 void populategrid (Variables *A) {
@@ -98,15 +101,10 @@ void populategrid (Variables *A) {
 		for (A->j = 0; A->j < N_COLS; ++A->j) {
 			getAngles(A);
 			A->areaGrid[A->i][A->j] = pow(RADIUS_PLANET,2) * sin(A->theta) * A->delta_theta *
-                                      A->delta_phi; //Finds the area of the element
+			A->delta_phi; //Finds the area of the element
 			area_total = area_total + A->areaGrid[A->i][A->j];
 		}
 	}
-	double area_expected = 4 * PI * pow(RADIUS_PLANET,2);
-
-	printf("The expected area of the planet (4.pi.R^2) is %g, and the summation of all the area "
-           "elements in our grid is %g. This gives a discrepancy (as a fraction) of %g.\n",
-           area_expected, area_total, fabs(area_total/area_expected - 1) );
 }
 
 void getAngles (Variables *A) {
@@ -119,17 +117,17 @@ void getVisibleArea (Variables *A) {
 }
 
 void getMu (Variables *A, double x, double y, double modPos_element) {
-	A->mu = cos( acos(-x / modPos_element) +
-				 acos((x*A->obsDistance_x + y*A->obsDistance_y) / (modPos_element * A->obsDistance)) );
-/* acos(mu) is the angle between the star and observer, which is the angle between the star and the
-   normal of the area element plus the angle between the normal of the area element and the observer.*/
+	A->mu = (x*A->obsDistance_x + y*A->obsDistance_y) / (modPos_element * A->obsDistance);
+	/* acos(mu) is the scattering angle, which is the angle between the normal to the surface element and the
+	   observer */
 }
 
 void getReceivedFlux (Variables *A) {
 	A->flux_received = A->flux_received + (A->intensity_element * A->mu * A->areaGrid[A->i][A->j]);
+	// Should divide by the square of the observer's distance, but it's just a scaling factor
 }
 
-double conductLambert (Variables *A) {
+void conductLambert (Variables *A) {
 	populategrid(A);
 
 	double x, y, z;         // Cartesian components of position of area element
@@ -146,11 +144,11 @@ double conductLambert (Variables *A) {
             modPos_element = sqrt(x*x + y*y + z*z);
 
             if ( -x / modPos_element > 0 && (x*A->obsDistance_x + y*A->obsDistance_y) /
-											(modPos_element * A->obsDistance) > 0 ) {
-/* The star is lying on the -x-axis: the first condition determines whether the angle between the
-   normal of the area element and star is < 90 degrees, if so it's illuminated. The second condition
-   tests whether the angle between the normal of the area element and the observer is < 90 degrees, if
-   so it's visible. */
+				                            (modPos_element * A->obsDistance) > 0 ) {
+				/* The star is lying on the -x-axis: the first condition determines whether the angle between the
+				 normal of the area element and star is < 90 degrees, if so it's illuminated. The second condition
+				 tests whether the angle between the normal of the area element and the observer is < 90 degrees, if
+				 so it's visible. */
 				getVisibleArea(A);
 				getMu(A, x, y, modPos_element);
                 A->intensity_element = A->mu;
@@ -159,24 +157,15 @@ double conductLambert (Variables *A) {
             }
 		}
     }
-    A->flux_received = A->flux_received / pow(A->obsDistance,2);
-    // Normalizes the total flux
-
-    printf("The total surface area of the planet that is both illuminated and visible is %g (equal to "
-           "%g.pi.R^2), and the total flux density received by the observer is %g.\n", A->area_visible,
-           A->area_visible / (PI * RADIUS_PLANET), A->flux_received);
-
-	return 0;
+	printf("The visible illuminated area is %g.pi.R^2.\n", A->area_visible / (PI * pow(RADIUS_PLANET,2)) );
 }
 
-double conductIso (Variables *A) {
+void conductIso (Variables *A) {
     populategrid(A);
     // DO CALCULATIONS
-	return 0;
 }
 
-double conductRayleigh (Variables *A) {
+void conductRayleigh (Variables *A) {
     populategrid(A);
     // DO CALCULATIONS
-	return 0;
 }
