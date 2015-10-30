173181fe2e7286461f17f91c9bb8e80c2527907f
Eshwen Bhal <milanman_eshb@hotmail.co.uk>
2015-10-30 11:58:03 +0000

    hai

--- Testing/Non-Monte Carlo tests/solid angle correction.c
+++ Testing/Non-Monte Carlo tests/solid angle correction.c
@@ -3,35 +3,45 @@
 #include <math.h>
 
 #define PI 3.14159265
+#define N_ROWS 180
+#define N_COLS 5 // 2 columns just for intensity, 5 if including polarization
 
 int main() {
     int m, n;
-    double olddata[181][2] = {0};
+    double olddata[N_ROWS][N_COLS] = {0};
 
     FILE *readfile;
-    readfile = fopen("ExOExRayleigh05.txt", "r"); // MODIFIED TO SUIT USER
+    readfile = fopen("ExOExRayleigh10.txt", "r"); // MODIFIED TO SUIT USER
 
     if ( readfile == NULL ) {
         printf("The file cannot be opened.\n");
         exit(1);
     }
 
-    for (m = 0 ; m < 180 ; ++m) {
-        for (n = 0 ; n < 2 ; ++n) {
+    for (m = 0 ; m < N_ROWS ; ++m) {
+        for (n = 0 ; n < N_COLS ; ++n) {
             fscanf(readfile, "%lg", &olddata[m][n]);
         }
     }
     fclose(readfile);
 
     double omega;
-    double newdata[181][2];
+    double newdata[N_ROWS][N_COLS];
     FILE *outfile;
-    outfile = fopen("ExOExRayleigh05new.txt", "w"); // MODIFIED TO SUIT USER
+    outfile = fopen("ExOExRayleigh10new.txt", "w"); // MODIFIED TO SUIT USER
 
-    for (m = 0 ; m < 180 ; ++m) {
+    for (m = 0 ; m < N_ROWS ; ++m) {
         omega = 2 * PI * ( cos(m*PI/180) - cos((m+1)*PI/180) );
         newdata[m][1] = olddata[m][1] / omega;
-        fprintf(outfile, "%i %lg\n", m, newdata[m][1] / newdata[0][1]);
+        if (N_COLS > 2) {
+            newdata[m][2] = olddata[m][2] / omega;
+            newdata[m][3] = olddata[m][3] / omega;
+            newdata[m][4] = olddata[m][4] / omega;
+            fprintf(outfile, "%i %lg %lg %lg %lg\n", m, newdata[m][1] / newdata[0][1],
+                    newdata[m][2], newdata[m][3], newdata[m][4]);
+        }
+        else
+            fprintf(outfile, "%i %lg\n", m, newdata[m][1] / newdata[0][1]);
     }
     fclose(outfile);
 
